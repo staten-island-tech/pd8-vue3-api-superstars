88e699aee26ab64eb79ce194f9f2e23efa862e2c
cailynkit <cailynkitchens8@gmail.com>
2023-05-11 13:53:15 -0400

    async sucks

--- vue-api/src/components/BarGraph.vue
+++ vue-api/src/components/BarGraph.vue
@@ -1,8 +1,47 @@
 <script setup>
-import { data } from "../api.js";
-import { Bar } from "vue-chartjs";
-import { ref } from "vue";
-
+import { Bar } from 'vue-chartjs'
+import { ref } from 'vue'
+let data, dataAges, dataActions
+let loaded = ref(false)
+async function name() {
+  data = await fetch('https://data.cityofnewyork.us/resource/vfnx-vebw.json')
+  data = await data.json()
+  data.map((squirrel) => {
+    squirrel.age = squirrel.age || 'Unknown'
+    return squirrel
+  })
+  dataAges = {
+    labels: ['Adult', 'Juvenile', 'Unknown'],
+    datasets: [
+      {
+        label: '',
+        backgroundColor: '#f87979',
+        data: [
+          data.filter((squirrel) => squirrel.age == 'Adult').length,
+          data.filter((squirrel) => squirrel.age == 'Juvenile').length,
+          data.filter((squirrel) => squirrel.age == 'Unknown').length
+        ]
+      }
+    ]
+  }
+  dataActions = {
+    labels: ['Chasing', 'Climbing', 'Eating', 'Foraging'],
+    datasets: [
+      {
+        label: '',
+        backgroundColor: ['#f87979', '#000', '#2a52be', '#4cf313'],
+        data: [
+          data.filter((squirrel) => squirrel.chasing == true).length,
+          data.filter((squirrel) => squirrel.climbing == true).length,
+          data.filter((squirrel) => squirrel.eating == true).length,
+          data.filter((squirrel) => squirrel.foraging == true).length
+        ]
+      }
+    ]
+  }
+  loaded.value = true
+}
+name()
 import {
   Chart as ChartJS,
   Title,
@@ -10,58 +49,19 @@ import {
   Legend,
   BarElement,
   CategoryScale,
-  LinearScale,
-} from "chart.js";
-
-ChartJS.register(
-  Title,
-  Tooltip,
-  Legend,
-  BarElement,
-  CategoryScale,
   LinearScale
-);
-data.map((squirrel) => {
-  squirrel.age = squirrel.age || "Unknown";
-  return squirrel;
-});
-let dataAges = {
-  labels: ["Adult", "Juvenile", "Unknown"],
-  datasets: [
-    {
-      label: "",
-      backgroundColor: "#f87979",
-      data: [
-        data.filter((squirrel) => squirrel.age == "Adult").length,
-        data.filter((squirrel) => squirrel.age == "Juvenile").length,
-        data.filter((squirrel) => squirrel.age == "Unknown").length,
-      ],
-    },
-  ],
-};
-let dataActions = {
-  labels: ["Chasing", "Climbing", "Eating", "Foraging"],
-  datasets: [
-    {
-      label: "",
-      backgroundColor: ["#f87979", "#000", "#2a52be", "#4cf313"],
-      data: [
-        data.filter((squirrel) => squirrel.chasing == true).length,
-        data.filter((squirrel) => squirrel.climbing == true).length,
-        data.filter((squirrel) => squirrel.eating == true).length,
-        data.filter((squirrel) => squirrel.foraging == true).length,
-      ],
-    },
-  ],
-};
-let prop = defineProps(["age"]);
-console.log(prop);
-let age = ref(prop.age);
+} from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+let prop = defineProps(['age'])
+console.log(prop)
+let age = ref(prop.age)
 </script>
 
 <template>
-  <Bar v-if="age == true" id="my-chart-id" :data="dataAges" />
-  <Bar v-else id="my-chart-id" :data="dataActions" />
+  <Bar v-if="age == true && loaded" id="my-chart-id" :data="dataAges" />
+  <Bar v-else-if="loaded" id="my-chart-id" :data="dataActions" />
 </template>
 
 <style></style>
--- vue-api/src/components/PieChart.vue
+++ vue-api/src/components/PieChart.vue
@@ -1,47 +1,58 @@
 <script setup>
-import { data } from "../api.js";
-import { Pie } from "vue-chartjs";
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Pie } from 'vue-chartjs'
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
+import { ref } from 'vue'
 
-ChartJS.register(ArcElement, Tooltip, Legend);
-data.map((squirrel) => {
-  squirrel.age = squirrel.age || "Unknown";
-  return squirrel;
-});
-let dataAges = {
-  labels: ["Adult", "Juvenile", "Unknown"],
-  datasets: [
-    {
-      backgroundColor: ["#f87979", "#000", "#2a52be"],
-      data: [
-        data.filter((squirrel) => squirrel.age == "Adult").length,
-        data.filter((squirrel) => squirrel.age == "Juvenile").length,
-        data.filter((squirrel) => squirrel.age == "Unknown").length,
-      ],
-    },
-  ],
-};
-let dataActions = {
-  labels: ["Chasing", "Climbing", "Eating", "Foraging"],
-  datasets: [
-    {
-      backgroundColor: ["#f87979", "#000", "#2a52be", "#4cf313"],
-      data: [
-        data.filter((squirrel) => squirrel.chasing == true).length,
-        data.filter((squirrel) => squirrel.climbing == true).length,
-        data.filter((squirrel) => squirrel.eating == true).length,
-        data.filter((squirrel) => squirrel.foraging == true).length,
-      ],
-    },
-  ],
-};
-let prop = defineProps(["age"]);
-let age = prop.age;
+let data, dataAges, dataActions
+let loaded = ref(false)
+async function name() {
+  data = await fetch('https://data.cityofnewyork.us/resource/vfnx-vebw.json')
+  data = await data.json()
+  data.map((squirrel) => {
+    squirrel.age = squirrel.age || 'Unknown'
+    return squirrel
+  })
+  dataAges = {
+    labels: ['Adult', 'Juvenile', 'Unknown'],
+    datasets: [
+      {
+        label: '',
+        backgroundColor: ['#f87979', '#000', '#2a52be'],
+        data: [
+          data.filter((squirrel) => squirrel.age == 'Adult').length,
+          data.filter((squirrel) => squirrel.age == 'Juvenile').length,
+          data.filter((squirrel) => squirrel.age == 'Unknown').length
+        ]
+      }
+    ]
+  }
+  dataActions = {
+    labels: ['Chasing', 'Climbing', 'Eating', 'Foraging'],
+    datasets: [
+      {
+        label: '',
+        backgroundColor: ['#f87979', '#000', '#2a52be', '#4cf313'],
+        data: [
+          data.filter((squirrel) => squirrel.chasing == true).length,
+          data.filter((squirrel) => squirrel.climbing == true).length,
+          data.filter((squirrel) => squirrel.eating == true).length,
+          data.filter((squirrel) => squirrel.foraging == true).length
+        ]
+      }
+    ]
+  }
+  loaded.value = true
+}
+name()
+ChartJS.register(ArcElement, Tooltip, Legend)
+
+let prop = defineProps(['age'])
+let age = prop.age
 </script>
 
 <template>
-  <Pie v-if="age == true" id="my-chart-id" :data="dataAges" />
-  <Pie v-else id="my-chart-id" :data="dataActions" />
+  <Pie v-if="age == true && loaded" id="my-chart-id" :data="dataAges" />
+  <Pie v-else-if="loaded" id="my-chart-id" :data="dataActions" />
   <!-- <Pie id="my-chart-id" :data="chartData" /> -->
 </template>
 
